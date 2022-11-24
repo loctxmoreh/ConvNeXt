43a83a662d3a55008a037c2d2725c534de67e5bc
Tran Xuan Loc <loc.tran@moreh.com.vn>
2022-11-24 12:59:43 +0900

    fix hardcoded directory path

--- finetune-single-node.sh
+++ finetune-single-node.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-python -m torch.distributed.launch \
+dataset_dir="/NAS/dataset/imagenet_100cls"
+[[ ! -d $dataset_dir ]] && echo "Dataset dir ${dataset_dir} not found" && exit 1
+
+output_dir="/data/work/convnext_output"
+[[ ! -d $output_dir ]] && echo "No output dir ${output_dir} found" && exit 1
+
+/usr/bin/env python3 -m torch.distributed.launch \
         --nproc_per_node=1 \
         main.py \
         --model convnext_base \
@@ -10,7 +16,7 @@ python -m torch.distributed.launch \
         --lr 5e-5 \
         --update_freq 2 \
         --head_init_scale 0.001 --cutmix 0 --mixup 0 \
-        --finetune /data/work/convnext_output/checkpoint-best-ema.pth \
-        --data_path /data/work/dataset/imagenet_100cls \
-        --output_dir /data/work/convnext_output \
+        --finetune $output_dir/checkpoint-best-ema.pth \
+        --data_path $dataset_dir \
+        --output_dir $output_dir \
         --warmup_epochs 0 --epochs 28
--- train-single-node.sh
+++ train-single-node.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-python -m torch.distributed.launch \
+dataset_dir="/NAS/dataset/imagenet_100cls"
+[[ ! -d $dataset_dir ]] && echo "Dataset dir ${dataset_dir} not found" && exit 1
+
+output_dir="/data/work/convnext_output"
+[[ ! -d $output_dir ]] && mkdir -p $output_dir
+
+/usr/bin/env python3 -m torch.distributed.launch \
         --nproc_per_node=1 \
         main.py \
         --model convnext_base \
@@ -9,6 +15,6 @@ python -m torch.distributed.launch \
         --lr 4e-3 \
         --update_freq 1 \
         --model_ema true --model_ema_eval true \
-        --data_path /data/work/dataset/imagenet_100cls \
-        --output_dir /data/work/convnext_output \
+        --data_path $dataset_dir \
+        --output_dir $output_dir \
         --epochs 25
